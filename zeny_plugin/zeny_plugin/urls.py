d2b7045dc66fd8f481d4912631885f4ceea9d56e
Brais Gabin <braisgabin@gmail.com>
2015-03-01 20:49:40 +0100

    Improve forms

--- zeny_plugin/zeny_plugin/urls.py
+++ zeny_plugin/zeny_plugin/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from app import views
+from oauth2_provider import views as oauth_views
 
 urlpatterns = patterns('',
     url(r'^user/me/storage/$', views.StorageList.as_view(), name='storage-list'),
+    url(r'^oauth/authorize/$', oauth_views.AuthorizationView.as_view(template_name='authorize.html'), name="authorize"),
+    url(r'^oauth/token/$', oauth_views.TokenView.as_view(), name="token"),
     url(r'^oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
 )
