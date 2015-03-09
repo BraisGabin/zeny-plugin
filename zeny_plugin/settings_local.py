966f11e414f4a64d7e1fcb560b53627e1bd02918
Brais Gabin <braisgabin@gmail.com>
2015-03-09 16:51:29 +0100

    Change project structure

---
+++ zeny_plugin/settings_local.py
@@ -0,0 +1,11 @@
+SECRET_KEY = '@3ieasv-%s7q84rlnxj4530i()=l$#r7oa*u0res4h(2%0os%z'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rathena',
+        'USER': 'zeny',
+        'PASSWORD': 'zeny',
+        'HOST': '54.171.17.18'
+    }
+}
