343bbffe1087809dd77c8af0955e6cdab174776a
Brais Gabin <braisgabin@gmail.com>
2015-03-10 13:44:59 +0100

    Refactor

--- zeny_plugin/app/managers.py
+++ zeny_plugin/app/managers.py
@@ -106,6 +106,128 @@ def check_no_char_online(cursor, user):
         raise ConflictError("You're connected to the server with the PJ %s, please disconnect and retry." % name)
 
 
+def check_user_have_items(cursor, user, items):
+    where, having, param_where, param_having = where_items_amount(items)
+
+    sql = """
+        SELECT
+            storage.nameid,
+            storage.refine,
+            storage.attribute,
+            storage.card0,
+            storage.card1,
+            storage.card2,
+            storage.card3,
+            SUM(storage.amount) as total_amount
+        FROM storage
+        WHERE
+        bound = 0 AND
+        expire_time = 0 AND
+        identify != 0 AND
+        account_id = %s AND
+        (""" + where + """)
+        GROUP BY nameid, refine, attribute, card0, card1, card2, card3
+        HAVING (""" + having + ")"
+
+    parameters = [user.pk]
+    parameters.extend(param_where)
+    parameters.extend(param_having)
+
+    cursor.execute(sql, parameters)
+    if cursor.rowcount != len(items):
+        raise ValidationError("You doesn't have this items.")  # TODO Verbose error. 409?
+
+
+def get_user_items(cursor, user, items):
+    where, having, param_where, param_having = where_items_amount(items)
+
+    sql = """
+        SELECT
+            storage.nameid,
+            storage.refine,
+            storage.attribute,
+            storage.card0,
+            storage.card1,
+            storage.card2,
+            storage.card3,
+            SUM(storage.amount) as total_amount,
+            item.type,
+            storage_vending.amount
+        FROM storage
+        INNER JOIN item_db_re AS item ON
+            storage.nameid = item.id
+        LEFT JOIN storage_vending ON
+            item.type NOT IN (4, 5, 7, 8, 12) AND
+            storage.nameid = storage_vending.nameid AND
+            storage.refine = storage_vending.refine AND
+            storage.attribute = storage_vending.attribute AND
+            storage.card0 = storage_vending.card0 AND
+            storage.card1 = storage_vending.card1 AND
+            storage.card2 = storage_vending.card2 AND
+            storage.card3 = storage_vending.card3
+        WHERE
+            storage.bound = 0 AND
+            storage.expire_time = 0 AND
+            storage.identify != 0 AND
+            storage.account_id = %s AND
+            (""" + where + """)
+        GROUP BY nameid, refine, attribute, card0, card1, card2, card3
+        HAVING (""" + having + ")"
+
+    parameters = [user.pk]
+    parameters.extend(param_where)
+    parameters.extend(param_having)
+
+    cursor.execute(sql, parameters)
+
+
+def where_items_amount(items):
+    where = []
+    having = []
+    param_where = []
+    param_having = []
+    for item in items:
+        where.append("""(
+            storage.nameid = %s AND
+            storage.refine = %s AND
+            storage.attribute = %s AND
+            storage.card0 = %s AND
+            storage.card1 = %s AND
+            storage.card2 = %s AND
+            storage.card3 = %s)
+        """)
+        having.append("""(
+            storage.nameid = %s AND
+            storage.refine = %s AND
+            storage.attribute = %s AND
+            storage.card0 = %s AND
+            storage.card1 = %s AND
+            storage.card2 = %s AND
+            storage.card3 = %s AND
+            SUM(storage.amount) >= %s)
+        """)
+        params = get_params_item(item)
+        param_where.extend(params)
+        param_having.extend(params)
+        param_having.append(item['amount'])
+
+    where = ' OR '.join(where)
+    having = ' OR '.join(having)
+    return where, having, param_where, param_having
+
+
+def get_params_item(item):
+    return [
+        item['nameid'],
+        item['refine'],
+        item['attribute'],
+        item['card0'],
+        item['card1'],
+        item['card2'],
+        item['card3']
+    ]
+
+
 class StorageManager(models.Manager):
     def get_queryset(self):
         return super(StorageManager, self).get_queryset().filter(bound='0', expire_time='0', ).exclude(identify='0')
@@ -115,9 +237,7 @@ class StorageManager(models.Manager):
 
         cursor = connection.cursor()
         check_no_char_online(cursor, user)
-        cursor.execute(*self.check_items_sql(user, items))
-        if cursor.rowcount != len(items):
-            raise ValidationError("You doesn't have this items.")  # TODO Verbose error. 409?
+        check_user_have_items(cursor, user, items)
 
     def move_items(self, user, items):
         from django.db import connection
@@ -131,7 +251,7 @@ class StorageManager(models.Manager):
                     `item_db_re` AS item LOW_PRIORITY WRITE,
                     `storage_vending` LOW_PRIORITY WRITE""")
             check_no_char_online(cursor, user)
-            cursor.execute(*self.check_items_sql2(user, items))
+            get_user_items(cursor, user, items)
             if cursor.rowcount != len(items):
                 raise ValidationError("You doesn't have this items.")  # TODO Verbose error. 409?
             for row in cursor.fetchall():
@@ -151,109 +271,3 @@ class StorageManager(models.Manager):
                         update_item_storage(cursor, user, item)
         finally:
             cursor.execute("UNLOCK TABLES")
-
-    @staticmethod
-    def check_items_sql(user, items):
-        where = []
-        having = []
-        param_where = []
-        param_having = []
-        for item in items:
-            where.append(
-                '(nameid = %s AND refine = %s AND attribute = %s AND card0 = %s AND card1 = %s AND card2 = %s AND card3 = %s)')
-            having.append(
-                '(nameid = %s AND refine = %s AND attribute = %s AND card0 = %s AND card1 = %s AND card2 = %s AND card3 = %s AND total_amount >= %s)')
-            params = [item['nameid'], item['refine'], item['attribute'], item['card0'], item['card1'], item['card2'],
-                      item['card3']]
-            param_where.extend(params)
-            param_having.extend(params)
-            param_having.append(item['amount'])
-
-        where = ' OR '.join(where)
-        having = ' OR '.join(having)
-        sql = """
-            SELECT nameid, refine, attribute, card0, card1, card2, card3, SUM(amount) as total_amount, COUNT(id) as rows
-            FROM storage
-            WHERE bound = 0 AND expire_time = 0 AND identify != 0
-            AND account_id = %s AND (""" + where + """)
-            GROUP BY nameid, refine, attribute, card0, card1, card2, card3
-            HAVING (""" + having + ")"
-
-        parameters = [user.pk]
-        parameters.extend(param_where)
-        parameters.extend(param_having)
-
-        return [sql, parameters]
-
-    @staticmethod
-    def check_items_sql2(user, items):
-        where = []
-        having = []
-        param_where = []
-        param_having = []
-        for item in items:
-            where.append("""(
-                storage.nameid = %s AND
-                storage.refine = %s AND
-                storage.attribute = %s AND
-                storage.card0 = %s AND
-                storage.card1 = %s AND
-                storage.card2 = %s AND
-                storage.card3 = %s)
-            """)
-            having.append("""(
-                storage.nameid = %s AND
-                storage.refine = %s AND
-                storage.attribute = %s AND
-                storage.card0 = %s AND
-                storage.card1 = %s AND
-                storage.card2 = %s AND
-                storage.card3 = %s AND
-                total_amount >= %s)
-            """)
-            params = [item['nameid'], item['refine'], item['attribute'], item['card0'], item['card1'], item['card2'],
-                      item['card3']]
-            param_where.extend(params)
-            param_having.extend(params)
-            param_having.append(item['amount'])
-
-        where = ' OR '.join(where)
-        having = ' OR '.join(having)
-        sql = """
-            SELECT
-                storage.nameid,
-                storage.refine,
-                storage.attribute,
-                storage.card0,
-                storage.card1,
-                storage.card2,
-                storage.card3,
-                SUM(storage.amount) as total_amount,
-                item.type,
-                storage_vending.amount
-            FROM storage
-            INNER JOIN item_db_re AS item ON
-                storage.nameid = item.id
-            LEFT JOIN storage_vending ON
-                item.type NOT IN (4, 5, 7, 8, 12) AND
-                storage.nameid = storage_vending.nameid AND
-                storage.refine = storage_vending.refine AND
-                storage.attribute = storage_vending.attribute AND
-                storage.card0 = storage_vending.card0 AND
-                storage.card1 = storage_vending.card1 AND
-                storage.card2 = storage_vending.card2 AND
-                storage.card3 = storage_vending.card3
-            WHERE
-                storage.bound = 0 AND
-                storage.expire_time = 0 AND
-                storage.identify != 0 AND
-                storage.account_id = %s AND
-                (""" + where + """)
-            GROUP BY nameid, refine, attribute, card0, card1, card2, card3
-            HAVING (""" + having + ")"
-
-        parameters = [user.pk]
-        parameters.extend(param_where)
-        parameters.extend(param_having)
-
-        return [sql, parameters]
